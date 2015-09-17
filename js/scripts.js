c6b9d1dafa9e173896062f4e0709d427ab7088a0
Nick Bernard <nick@nrbernard.com>
2015-09-16 20:24:36 -0700

    Update wage breakdown on slider change

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
-$('#wage-slider').slider({
-  formatter: function(value) {
-    return '$' + value;
-  },
-  tooltip: 'always',
-  ticks: [9.25, 25],
-  ticks_labels: ['$9.25', '$25']
-});
-
-
-$('#map-slider').slider({
-  formatter: function(value) {
-    return 'Current value: ' + value;
+$(function() {
+  function updateBreakdown(value) {
+    $('span.hour').text(value);
+    $('span.day').text(value * 8);
+    $('span.week').text(value * 8 * 5);
+    $('span.month').text(value * 8 * 5 * 4);
+    $('span.year').text(value * 8 * 5 * 4 * 12);
   }
+
+  $( '#wage-slider' ).slider({
+    formatter: function(value) {
+      updateBreakdown(value);
+      return '$' + value;
+    },
+    tooltip: 'always',
+    ticks: [9.25, 25],
+    ticks_labels: ['$9.25', '$25']
+  });
+
+  $('#map-slider').slider({
+    formatter: function(value) {
+      return 'Current value: ' + value;
+    }
+  });
 });
